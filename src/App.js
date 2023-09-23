1328b5638be185b048ad4ab156dd0660d15cab89
Sauloteles <105803651+Saulot-dev@users.noreply.github.com>
2023-09-23 12:54:19 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
-import logo from './logo.svg';
+/*import logo from './logo.svg';*/
 import Table from './Table';
 import Form from './Form';
 
